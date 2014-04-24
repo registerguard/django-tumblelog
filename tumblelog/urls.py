ca9518254bd85503115be53efc3e2b71ac3b8267
J. Heasly <jheasly@earthlink.net>
2014-04-23 18:49:19 -0700

    Fixes django.conf.urls import
    django.conf.urls.defaults has been removed: https://docs.djangoproject.com/en/1.6/internals/deprecation/

--- tumblelog/urls.py
+++ tumblelog/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import include, patterns, url
 
 from tumblelog.feeds import PostFeed
 from tumblelog.views import PostDetailView, PostListView
